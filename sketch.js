3ba66e236b49e282ab9555da0c6651d42b244f68
icegamer910 <101666859+icegamer910@users.noreply.github.com>
2022-08-11 15:58:50 -0300

    11/8

--- sketch.js
+++ sketch.js
@@ -1,15 +1,22 @@
 var canvas;
 var backgroundImage, car1_img, car2_img, track;
+var fuelImage, powerCoinImage;
+var obstacle2Image
+var obstacle1Image
 var database, gameState;
 var form, player, playerCount;
-var allPlayers, car1, car2;
+var allPlayers, car1, car2, fuels, powerCoins, obstacles;
 var cars = [];
 
 function preload() {
   backgroundImage = loadImage("./assets/planodefundo.png");
-  car1_img = loadImage("../assets/car1.png");
-  car2_img = loadImage("../assets/car2.png");
-  track = loadImage("../assets/pista.png");
+  car1_img = loadImage("./assets/car1.png");
+  car2_img = loadImage("./assets/car2.png");
+  track = loadImage("./assets/PISTA.png");
+  fuelImage = loadImage("./assets/fuel.png");
+  powerCoinImage = loadImage("./assets/goldCoin.png");
+  obstacle2Image = loadImage("./assets/obstacle2.png")
+  obstacle1Image = loadImage("./assets/obstacle1.png")
 }
 
 function setup() {
